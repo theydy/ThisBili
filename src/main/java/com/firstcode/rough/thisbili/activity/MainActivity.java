3057a1d4c6cc08c3de60c550d3deeb1eafa37f2e
rough <342730179@qq.com>
2016-05-21 20:00:24 +0800

    first commit

---
+++ src/main/java/com/firstcode/rough/thisbili/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.firstcode.rough.thisbili.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firstcode.rough.thisbili.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
